48ee60099c9b1446a1e1402cbe2c8ca08c9948b7
Vitor Borges <vitorsbgs@gmail.com>
2025-05-05 20:18:25 -0300

    feat: initialize Angular application with i18n support

---
+++ src/app/app.config.ts
@@ -0,0 +1,20 @@
+import { ApplicationConfig, isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@jsverse/transloco';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), provideHttpClient(), provideTransloco({
+        config: { 
+          availableLangs: ['en', 'es'],
+          defaultLang: 'en',
+          // Remove this option if your application doesn't support changing language in runtime.
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      })]
+};
